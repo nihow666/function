bc0de50a567ccd7b5d87c713aa3f59ef2fe24420
solomeowl <j19940430@gmail.com>
2021-05-12 15:25:04 +0800

    finished

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
 fn main() {
     println!("Hello, world!");
+    another_function(444, 666);
+    statment_function();
+}
+
+fn another_function(x: i32, y: i32) {
+    println!("x = {}", x);
+    println!("y = {}", y);
+}
+
+fn statment_function() {
+    //unused variable
+    let x = 5;
+
+    let y = {
+        let x = 3;
+        x + 1
+    };
+
+    println!("y 的數值為：{}", y);
 }
